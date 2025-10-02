63cf578ae9bd4cae12f63f482be9145798a231f1
raspberrypi5gaming-blip <raspberrypi5gaming@gmail.com>
2025-10-02 15:26:27 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const WebSocket = require('ws');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.send('Hello from WebSocket server!');
+
+  ws.on('message', (msg) => {
+    console.log('Received:', msg);
+    ws.send('You said: ' + msg);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('WebSocket server is running!');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
